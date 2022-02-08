d9a286ca8d9b35647a8dc9bfaa79c0ca525e57db
Joseph Fulkerson <josephafulkerson@gmail.com>
2022-02-07 22:54:03 -0500

    first commit

---
+++ data.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+points_scored = [90, 94, 53, 89, 80, 86, 69, 52, 76, 96, 77, 68, 66, 67, 66, 79, 68, 64, 78, 51, 90, 81]
+plt.plot(points_scored, linewidth=5)
+plt.show()
