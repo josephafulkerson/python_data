729eb28d38d9632b37e527e225b40ec1a528de9f
Joseph Fulkerson <josephafulkerson@gmail.com>
2022-02-09 13:24:36 -0500

    plotting scatter plots

---
+++ practice/practice_data.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, s=40, edgecolor='none', c=y_values, cmap=plt.cm.Oranges)
+plt.title("Practice Chart", fontsize=22)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+plt.axis([0,1100, 0, 1100000])
+plt.show()
